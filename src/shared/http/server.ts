c5a955fa927de036496e3feeb3cd953495b56901
Marcello Cavazza <marcellocavazzaoliveira@gmail.com>
2022-06-14 15:02:40 -0300

    setting up middleware to treat errors

---
+++ src/shared/http/server.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import express, { NextFunction } from 'express';
+import cors from 'cors';
+import routes from './routes/index';
+import AppError from '../errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  },
+);
+
+app.listen(5000);
